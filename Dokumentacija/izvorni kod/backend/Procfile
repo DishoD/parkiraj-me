dd1a6580c7bf8bfcf2f36f7e82afdbe7ba12ea3d
Hrvoje Ditrih <hrvoje.ditrih@fer.hr>
2019-01-16 13:23:23 +0100

    deploy na heroku2

---
+++ Dokumentacija/izvorni kod/backend/Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/projekt-0.0.1-SNAPSHOT.jar
---
+++ Dokumentacija/izvorni kod/backend/src/main/resources/public/precache-manifest.4fca0dcdb17e35ba77798f27e852b1ea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "886483f7cc91c494cc73",
+    "url": "/static/css/main.7f8ce279.chunk.css"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "20a38799a9441e4cb2e9",
+    "url": "/static/js/1.20a38799.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "886483f7cc91c494cc73",
+    "url": "/static/js/main.886483f7.chunk.js"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/static/media/rw-widgets.eceddf47.ttf"
+  },
+  {
+    "revision": "20a38799a9441e4cb2e9",
+    "url": "/static/css/1.71bdb180.chunk.css"
+  },
+  {
+    "revision": "111b9dd30ee78e14fd9a5f297fe422a5",
+    "url": "/index.html"
+  }
+];
